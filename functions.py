abef9f0d798da89ce72c06213b1b95c90513856d
Geraldine Aillon <geraldine.aillon1901@alumnos.ubiobio.cl>
2024-04-06 01:10:22 -0300

    added features to showOne()

--- functions.py
+++ functions.py
@@ -4,10 +4,10 @@ from bson import errors
 import pprint 
 import datetime
 
-#Coneccion a la base de datos 
+#se obtiene la base de datos sobre la cual se trabajará 
 main_db=client.guia1
 
-#Se asigna la coleccion con la que se va a trabajar
+#Se asigna la coleccion con la que se van a ejecutar las consultas
 collection = main_db.books
 
 #Obtenemos el año del sistema para evitar conflictos con las fechas
@@ -32,8 +32,8 @@ def addOne():
         except ValueError:
             print("Enter the whole year in numbers please(e.g:1990)")
 
-    movieadp=input("Movie Adaptation?: ")
-    pub=input("Publisher: ")
+    movieadp=input("Does it have a movie adaptation?(yes/no): ")
+    pub=input("Original publisher: ")
     gen=input("Genres (if more than one, separate by commas in the same line): ")
     try:
         record={
@@ -49,18 +49,32 @@ def addOne():
     except errors.BSONError:
         print("An error ocurred, please try again")
     else:
-        print("\nYour book was saved sucesfully :)\nHere are the details: \n")
+        print("\nYour book was saved sucesfully :) Here are the details: \n")
         pprint.pprint(book,sort_dicts=False )
 
     return(False)
 
-#op 2
-def showOne(book_id):
+#op 2: Muestra un solo libro por
+#titulo
+def showOnebyTitle(book_name):
+    query={"Title": {"$regex": book_name,"$options": "i"}}
+    book=collection.find(query)
+    if(book):
+        for aux in book:
+            pprint.pprint(aux, sort_dicts=False)
+    else:
+        print("The book you're lookign for doesn't exist in the database")
+        ad=input(" Would you like to add it?(y/n): ")
+        if(ad=="y"): addOne()
+    return(False)
+
+#id
+def showOneById(book_id):
     try:
         _id=ObjectId(book_id)
         book= collection.find_one({"_id":_id})
     except errors.InvalidId:
-        print("\n\t! ! ! ! ! ! ! ! !Invalid id format! ! ! ! ! ! ! ! !\n")
+        print("\n\t! ! ! ! ! ! ! ! !\tInvalid id format\t! ! ! ! ! ! ! ! !\n")
     else:
         if book:
             print("\n")
@@ -68,6 +82,17 @@ def showOne(book_id):
             print("\n")
         else:
                 print("\nThe book you're looking for doesn't exist in the database :(\n")
+    return(False)
+
+#autor
+def showOnebyAuthor(author):
+    query={"Author": {"$regex": author,"$options": "i"}}
+    books=collection.find(query)
+    if(books):
+        for aux in books:
+            pprint.pprint(aux, sort_dicts=False)
+    else:
+        print("The author you're lookign for doesn't exist in the database")
     return(False)
 
 #op 3 
@@ -137,13 +162,14 @@ def update_by_id(book_id):
         pprint.pprint(collection.find_one({"_id":_id}),sort_dicts=False)
         return(False)
 
-#op 4
+#op 4: Borra un documento de la colección segun el id entregado, si el id no existe o esta incorrecto
+#      se vuelve al menu principal
 def delete_by_id(book_id):
     try:
         _id=ObjectId(book_id)
         aux=collection.delete_one({"_id":_id})
     except errors.InvalidId:
-        print("! ! ! ! ! ! ! ! !\nInvalid id format\nnothing was deleted\n! ! ! ! ! ! ! ! !")
+        print("! ! ! ! ! ! ! ! !Invalid id format! ! ! ! ! ! ! ! !\nnothing was deleted\n")
     else:
         if(aux):
             print("\nDocument deleted succesfully")
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from functions import showAll,delete_by_id,showOne,update_by_id,addOne
+from functions import showAll,delete_by_id,showOneById,update_by_id,addOne,showOnebyTitle,showOnebyAuthor
 
 #Menu
 
 print("\nThis is a book directory, you can search for a specific book, add new books,\ndelete existing ones,update them and see all the books available.\n")
 
 def menu():
-    print("/*/ MAIN MENU /*/\n")
+    print("\n/*/ MAIN MENU /*/\n")
     print("1: Add new book\n2: Look for a book\n3: Edit book\n4: Delete book\n5: Show all books\n6: Quit\n")
     while True:
-            try:
-                option= int(input("Type the number of the option you want: "))
-                break
-            except ValueError:
-                print("Invalid character, please enter a number")
+                try:
+                        option= int(input("Type the number of the option you want: "))
+                        break
+                except ValueError:
+                        print("Invalid character, please enter a number")
                 
     state=True
     while state==True:
@@ -20,9 +20,30 @@ def menu():
                 print("\n")
                 state=addOne()
         elif option==2:
-                print("\n")
-                id=input("Enter the book id: ")
-                state=showOne(id)
+                print("1. Search by ID\n2. Search by tittle\n3. Search by author")
+                while True:
+                        try:
+                                opt=int(input("?: "))
+                                
+                        except ValueError:
+                                print("!!!\t Just type the number of the option please")
+                        else:
+                                if(opt==1):
+                                        id=input("Enter the book id: ")
+                                        state=showOneById(id)
+                                        break
+                                elif(opt==2):
+                                        title=input("Ente the title of the book: ")
+                                        state=showOnebyTitle(title)
+                                        break
+                                elif(opt==3):
+                                        author=input("Enter the Author's name: ")
+                                        state=showOnebyAuthor(author)
+                                        break
+                                else:
+                                      print("!!!\t Enter a VALID option") 
+                                      continue 
+                                
         elif option==3:
                 print("\n")
                 id=input("Enter the book id: ")
@@ -37,7 +58,7 @@ def menu():
         elif option==6:
                 print("\n")
                 print("Exiting...")
-                exit()
+                exit()             
         else:
             while True:
                 try:
